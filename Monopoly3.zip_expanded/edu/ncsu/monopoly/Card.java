52ce75fb5670386b12fd85b0c38b5c644caa041e
Jon Ander <Jon Ander@10.110.219.68>
2020-10-09 12:58:41 +0200

    Card Commentd Added

--- Monopoly3.zip_expanded/edu/ncsu/monopoly/Card.java
+++ Monopoly3.zip_expanded/edu/ncsu/monopoly/Card.java
@@ -8,4 +8,5 @@ public abstract class Card {
     public abstract String getLabel();
     public abstract void applyAction();
     public abstract int getCardType();
+    //Some random new comment I decided to add idk why my life is so tragic
 }
